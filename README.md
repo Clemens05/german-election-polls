b4da27bc5fe22993e3789f0bc4f99c0811bfec04
Clemens05 <clemensrustemeier@gmail.com>
2022-11-21 19:46:14 +0100

    added more information to the readme

--- README.md
+++ README.md
@@ -14,7 +14,16 @@ npm install german-election-polls
 import polls from 'german-election-polls';
 
 (async () => {
-  polls.shared.update();
+  await poll.shared.update();
+
+  const query = poll.shared.select([
+    Query.include([DataType.Surveys]),
+    Query.Survey.Tasker.ID.isNot([4, 6]),
+    Query.Survey.Release.isGreater(new Date('2021-11-21')),
+    Query.Survey.Parliament.Shortcut.is(['Nordrhein-Westfalen (NRW)', 'Baden-Württemberg']),
+    Query.Survey.Sort.byParticipants(Order.Asc),
+    Query.Survey.Sort.allResults(Order.Desc),
+  ]);
 })();
 import poll from 'german-election-polls';
 ```
--- src/__tests__/Poll.test.ts
+++ src/__tests__/Poll.test.ts
@@ -5,13 +5,11 @@ import { DataType, Order } from '../query';
 test('Query', async () => {
   await poll.shared.update();
 
-  let query = poll.shared.select([
+  const query = poll.shared.select([
     Query.include([DataType.Surveys]),
     Query.Survey.Tasker.ID.isNot([4, 6]),
     Query.Survey.Release.isGreater(new Date('2021-11-21')),
     Query.Survey.Parliament.Shortcut.is(['Nordrhein-Westfalen (NRW)', 'Baden-Württemberg']),
-    // Query.Survey.Institute.Name.is(['Forschungsgruppe Wahlen']),
-
     Query.Survey.Sort.byParticipants(Order.Asc),
     Query.Survey.Sort.allResults(Order.Desc),
   ]);
