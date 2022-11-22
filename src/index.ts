c1916e6a50cda7d2ce84527b8e0dd5113ada5cf7
Clemens05 <clemensrustemeier@gmail.com>
2022-11-22 13:34:46 +0100

    added more exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
+import { Institute } from './models/Institute';
+import { Parliament } from './models/Parliament';
+import { Party } from './models/Party';
+import { PollData } from './models/PollData';
+import { Survey } from './models/Survey';
+import { SurveyResult } from './models/SurveyResult';
+import { Tasker } from './models/Tasker';
 import { PollHandler } from './pollHandler';
 import { DataType, Order, Query } from './query';
 
-export { PollHandler as default, PollHandler as Polls, Query, DataType, Order };
+export {
+  PollHandler as default,
+  PollHandler as Polls,
+  Query,
+  DataType,
+  Order,
+  Institute,
+  Parliament,
+  Party,
+  PollData,
+  Survey,
+  SurveyResult,
+  Tasker,
+};
